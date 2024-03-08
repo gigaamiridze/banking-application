0dd4017bfb8c6f1d861214101db1e6c935835a90
Giga Amiridze <giga.amiridze@azry.com>
2024-03-09 00:48:24 +0400

    Added account mapper and dto

---
+++ src/main/java/com/workshop/banking/dto/AccountDto.java
@@ -0,0 +1,16 @@
+package com.workshop.banking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDto {
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+}
---
+++ src/main/java/com/workshop/banking/mapper/AccountMapper.java
@@ -0,0 +1,26 @@
+package com.workshop.banking.mapper;
+
+import com.workshop.banking.dto.AccountDto;
+import com.workshop.banking.entity.Account;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountMapper {
+
+    public Account mapToEntity(AccountDto accountDto) {
+        return Account.builder()
+                .id(accountDto.getId())
+                .accountHolderName(accountDto.getAccountHolderName())
+                .balance(accountDto.getBalance())
+                .build();
+    }
+
+    public AccountDto mapToDto(Account account) {
+        return AccountDto.builder()
+                .id(account.getId())
+                .accountHolderName(account.getAccountHolderName())
+                .balance(account.getBalance())
+                .build();
+    }
+
+}
